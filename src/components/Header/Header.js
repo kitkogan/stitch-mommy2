6638a339f5b25456243d5dc4057b27e5c6c7530b
Kit Kogan <kkogan.dev@gmail.com>
2020-11-01 21:17:45 -0600

    add project files and set up header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+            STITCH MOMMY BITCHES
+            </h1>
+          </header>
+        </div>
+      );
+    }
+  }
+
+  export default Header;
