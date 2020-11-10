d5d2195240addc48ac0aa2e8c7dd033a70a75c79
Kit Kogan <kkogan.dev@gmail.com>
2020-11-09 22:34:22 -0600

    style swal

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -47,7 +47,7 @@ class App extends Component {
       text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
+      confirmButtonColor: '#c671e7',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
